b6f35905069307b11cb2f0d2b22613ff7a8132d1
achref <achref.maarfi0@gmail.com>
2023-09-04 03:09:06 +0100

    login page design + provider layout prepared

--- client/src/app/layouts/auth-layout/auth-layout.module.ts
+++ client/src/app/layouts/auth-layout/auth-layout.module.ts
@@ -3,19 +3,21 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthLayoutRoutes } from './auth-layout.routing';
-import { LoginComponent } from '../../pages/login/login.component';
-import { RegisterComponent } from '../../pages/register/register.component';
+import { LoginComponent } from '../../auth/login/login.component';
+import { RegisterComponent } from '../../auth/register/register.component';
+import { ComponentsModule } from "../../components/components.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(AuthLayoutRoutes),
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ]
+    declarations: [
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(AuthLayoutRoutes),
+        FormsModule,
+        ReactiveFormsModule,
+        ComponentsModule
+    ]
 })
 export class AuthLayoutModule { }
